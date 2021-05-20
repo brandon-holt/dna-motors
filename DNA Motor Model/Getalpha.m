4ec9b32470b2ec2d7063b606aa0ca08d1f4c7b0c
arshiyanamazi <78523238+arshiyanamazi@users.noreply.github.com>
2021-05-20 15:46:09 -0400

    added function to get alpha.

---
+++ DNA Motor Model/Getalpha.m
@@ -0,0 +1,16 @@
+function alpha = Getalpha(t,p)
+tracks{1} = [t,p];    
+
+ma = msdanalyzer(2, 'µm', 's');
+ma = ma.addAll(tracks);
+
+
+ma = ma.computeMSD;
+
+ma = ma.fitLogLogMSD(0.50);
+
+alpha = ma.loglogfit.alpha;
+
+end
+
+
