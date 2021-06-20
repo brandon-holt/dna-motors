36dee1b308aefb3eb81b4586982c4310c001e123
arshiyanamazi <78523238+arshiyanamazi@users.noreply.github.com>
2021-06-20 15:39:16 -0400

    Fixed function such that everything was plotted
    Script now plots all constants vs speed and processivity( details in comments of the script: Figures 1-3 = Kon, Koff, Kcat vs speed, Figures 4-6 = Kon, Koff, Kcat vs processivity.) I have no idea how to explain the results. Need to discuss with Brandon

--- DNA Motor Model/plottimestep.m
+++ DNA Motor Model/plottimestep.m
@@ -1,18 +1,46 @@
-x10 = ten(1:50,1);
-y10 = ten(1:50,4);
-plot(x10,y10,'Color', 'blue')
+%Figure 1-3 is Kon, Koff, Kcat vs speed. Figure 4-6 is Kon, Koff, Kcat vs
+%processivity
+x = logspace(-2,3,50);
+L = 1;
+counter = 1;
+speed = 4;
+processivity = 5;
+for i = [50,100,150]
+   
+    y10 = ten(L:i,speed);
+    figure(counter); plot(x,y10,'Color', 'blue')
+    
+    hold on
 
-hold on
+    y1 = one(L:i,speed);
+    plot(x,y1,'Color', 'red')
 
-x1 = one(1:50,1);
-y1 = one(1:50,4);
-plot(x1,y1,'Color', 'red')
+    y0 = onetenth(L:i,speed);
+    plot(x,y0,'Color', 'green')
 
-x0 = onetenth(1:50,1);
-y0 = onetenth(1:50,4);
-plot(x0,y0,'Color', 'green')
+    y0 = onehundreth(L:i,speed);
+    plot(x,y0,'Color', 'black')
+    hold off
+    L = i+1;
+    counter = counter +1;
+end
+L = 1;
+for i = [50,100,150]
+   
+    y10 = ten(L:i,processivity);
+    figure(counter); plot(x,y10,'Color', 'blue')
+    
+    hold on
 
-x0 = onehundreth(1:50,1);
-y0 = onehundreth(1:50,4);
-plot(x0,y0,'Color', 'black')
-hold off
+    y1 = one(L:i,processivity);
+    plot(x,y1,'Color', 'red')
+
+    y0 = onetenth(L:i,processivity);
+    plot(x,y0,'Color', 'green')
+
+    y0 = onehundreth(L:i,processivity);
+    plot(x,y0,'Color', 'black')
+    hold off
+    L = i+1;
+    counter = counter +1;
+end
